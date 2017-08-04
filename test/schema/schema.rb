63349302b9609d5b48f30bc628c9edda2524f764
yo_waka <y.wakahara@gmail.com>
2017-08-05 01:00:21 +0900

    use null false

--- test/schema/schema.rb
+++ test/schema/schema.rb
@@ -2,15 +2,15 @@ ActiveRecord::Schema.define do
   create_table :rents, force: true do |t|
     t.integer :book_id
     t.integer :user_id
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table :books, force: true do |t|
     t.integer :user_id
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table :users, force: true do |t|
-    t.timestamps
+    t.timestamps null: false
   end
 end
