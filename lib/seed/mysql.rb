11f670a1b65c6d712155105947f50ac26eb1c2d8
keik <keik@c-fo.com>
2023-11-22 13:09:01 +0900

    fix: Consider DB connection port.

--- lib/seed/mysql.rb
+++ lib/seed/mysql.rb
@@ -10,13 +10,13 @@ module Seed
           ''
         end
 
-      cmd = "MYSQL_PWD=#{password} mysqldump -u #{username} -h #{host} #{database} -t #{allow_tables_option(tables)} #{ignore_tables_option(ignore_tables)} --no-tablespaces #{additional_options} > #{file_path}"
+      cmd = "MYSQL_PWD=#{password} mysqldump -u #{username} -h #{host} -P #{port} #{database} -t #{allow_tables_option(tables)} #{ignore_tables_option(ignore_tables)} --no-tablespaces #{additional_options} > #{file_path}"
       system cmd
     end
 
     def self.restore(file_path, username:, password:, host:, port:, database:)
       host = 'localhost' unless host
-      cmd = "MYSQL_PWD=#{password} mysql -u #{username} -h #{host} #{database} < #{file_path}"
+      cmd = "MYSQL_PWD=#{password} mysql -u #{username} -h #{host} -P #{port} #{database} < #{file_path}"
       system cmd
     end
 
